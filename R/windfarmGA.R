ee7caa4d5ec2a742d122d25dc4e36d909935f32f
YsoSirius <kona1@gmx.at>
2017-06-25 22:13:26 +0200

    proj4string-glob.Var

--- R/windfarmGA.R
+++ R/windfarmGA.R
@@ -180,7 +180,7 @@ windfarmGA <- function(dns,layer,Polygon1,Projection,sourceCCL,sourceCCLRoughnes
     cat("Polygon is not projected. Lambert Azimuthal Equal Area Projection is used.\n")
     Projection = "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000
     +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-    proj4string(Polygon1) <- CRS(Projection)
+    sp::proj4string(Polygon1) <- CRS(Projection)
   }
   if (missing(Projection)) {
     cat("No Projection is given. Take Lambert Azimuthal Equal Area Projection.\n")
