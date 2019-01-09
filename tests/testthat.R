13bc931b78f90ba301bf2f40037474c8546497ca
YsoSirius <kona1@gmx.at>
2019-01-09 22:50:47 +0100

    images

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(windfarmGA)
+
+test_check("windfarmGA")
---
+++ tests/testthat/test_test.R
@@ -0,0 +1,8 @@
+context("String length")
+library(stringr)
+
+# test_that("str_length is number of characters", {
+#   expect_equal(str_length("a"), 1)
+#   expect_equal(str_length("ab"), 2)
+#   expect_equal(str_length("abc"), 3)
+# })
