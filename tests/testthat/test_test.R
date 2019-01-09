7725882f984fed5cc73c4e08d89840b69bba3d6e
YsoSirius <kona1@gmx.at>
2019-01-10 00:27:30 +0100

    gitignore

--- tests/testthat/test_test.R
+++ tests/testthat/test_test.R
@@ -1,8 +1,8 @@
-context("String length")
-library(stringr)
+# context("String length")
+library(testthat)
+library(windfarmGA)
 
-# test_that("str_length is number of characters", {
-#   expect_equal(str_length("a"), 1)
-#   expect_equal(str_length("ab"), 2)
-#   expect_equal(str_length("abc"), 3)
-# })
+test_that("BaroHoehe works", {
+  data <- matrix(seq(0,5000,500));  
+  expect_s3_class(BaroHoehe(data), "data.frame")
+})
