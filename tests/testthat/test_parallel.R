9fa39c42532a7713520bb85845ba69b6afbb081c
YsoSirius <kona1@gmx.at>
2019-07-19 02:32:21 +0200

    test parallel

--- tests/testthat/test_parallel.R
+++ tests/testthat/test_parallel.R
@@ -4,6 +4,7 @@ library(sp)
 library(windfarmGA)
 
 test_that("Test Parallelisation", {
+  ## Inputs ##################
   Polygon1 <- Polygon(rbind(c(4498482, 2668272), c(4498482, 2669343),
                       c(4499991, 2669343), c(4499991, 2668272)))
   Polygon1 <- Polygons(list(Polygon1),1);
@@ -15,16 +16,38 @@ test_that("Test Parallelisation", {
   Grid1 <- grid_area(shape = Polygon1,resol = 200,prop = 1);
   Grid <- Grid1[[1]]
   AmountGrids <- nrow(Grid)
-
   wind <- list(wind, probab = 100)
+  ##################
+  
   startsel <- init_population(Grid,10,20);
   fit <- fitness(selection = startsel, referenceHeight = 100, RotorHeight=100,
                  SurfaceRoughness=0.3,Polygon = Polygon1, resol1 = 200,rot=20,
                  dirspeed = wind, srtm_crop="", topograp=FALSE, cclRaster="",
                  Parallel = TRUE)
+  
   expect_output(str(fit), "List of 20")
   expect_true(all(sapply(fit, nrow) == 10))
   expect_false(any(unlist(sapply(fit, is.na))))
   expect_false(any(unlist(do.call("rbind", fit)[,-c(1,2)] < 0)))
+  
+  
+  res <- genetic_algorithm(Polygon1 = Polygon1,
+                           n = 12,
+                           vdirspe = wind,
+                           Rotor = 30,
+                           RotorHeight = 100, Parallel = TRUE)
+  expect_true(nrow(res) == 20)
+  expect_is(res, "matrix")
+  expect_false(any(unlist(sapply(res, is.na))))
+  
+  res <- genetic_algorithm(Polygon1 = Polygon1,
+                           n = 12,
+                           vdirspe = wind,
+                           Rotor = 30,
+                           RotorHeight = 100, 
+                           Parallel = TRUE, numCluster = 4)
+  expect_true(nrow(res) == 20)
+  expect_is(res, "matrix")
+  expect_false(any(unlist(sapply(res, is.na))))
   
 })
