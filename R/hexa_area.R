4c89c839893fa9c2fa4f4ef5b46f2c0acade9dae
YsoSirius <kona1@gmx.at>
2019-07-26 01:09:06 +0200

    fix

--- R/hexa_area.R
+++ R/hexa_area.R
@@ -10,27 +10,7 @@
 #'
 #' @family Helper Functions
 #' @return Returns a SpatialPolygons. (SpatialPolygons)
-#'   
-#' @examples \dontrun{
-#' library(spatstat)
-#' library(maptools)
-#' library(sp)
-#' Polygon1 <- Polygon(rbind(c(4498482, 2668272), c(4498482, 2669343),
-#'                           c(4499991, 2669343), c(4499991, 2668272)))
-#' Polygon1 <- Polygons(list(Polygon1),1);
-#' Polygon1 <- SpatialPolygons(list(Polygon1))
-#' Projection <- "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000
-#' +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-#' proj4string(Polygon1) <- CRS(Projection)
 #' 
-#' ## Create a hexagonal Tesselation
-#' HexaGrid <- spatstat::hextess(maptools::as.owin.SpatialPolygons(Polygon1),s = 100)
-#' plot(HexaGrid)
-#' 
-#' ## Convert the Tesselation to SpatialPolygons
-#' Hex2spdf <- tess2SPdf(HexaGrid)
-#' plot(Hex2spdf)
-#' }
 tess2SPdf <- function(x) {
   stopifnot(spatstat::is.tess(x))
   sp.obj <- methods::as(x, "SpatialPolygons")
