8a6eaa74314f505b2903763ca6679512d0baf968
Sebastian Gatscha <gatscha@trafficon.eu>
2021-07-23 11:40:05 +0200

    GHA, skip terrain-test if offline

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // wake_CPP
 double wake_CPP(double Rotorf, double wakr, double leA);
 RcppExport SEXP _windfarmGA_wake_CPP(SEXP RotorfSEXP, SEXP wakrSEXP, SEXP leASEXP) {
